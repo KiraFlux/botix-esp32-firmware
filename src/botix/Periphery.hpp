90d570a776c7f8634498c85c13fe1fdb5f4e87a1
KiraFlux <pret2titio1n@gmail.com>
2026-04-26 19:48:51 +0300

    update: Periphery: add WheelOdometerEncoders

--- src/botix/Periphery.hpp
+++ src/botix/Periphery.hpp
@@ -13,11 +13,13 @@ namespace internal {
 
 struct PeripheryConfig final : kf::mixin::NonCopyable {
 
-    MotorDriver::PwmOutputImpl::Config motor_left_pwm_forward, motor_left_pwm_backward, motor_right_pwm_forward, motor_right_pwm_backward;
+    MotorDriver::PwmOutputImpl::Config motor_driver_left_pwm_forward, motor_driver_left_pwm_backward, motor_driver_right_pwm_forward, motor_driver_right_pwm_backward;
 
     MotorDriver::Config motor_driver;
 
-    static constexpr MotorDriver::PwmOutputImpl::Config createMotorPwmOutputConfig(gpio_num_t pin, kf::u8 channel) noexcept {
+    WheelOdometerEncoder::Config wheel_odometry_encoder_left, wheel_odometry_encoder_right;
+
+    static constexpr MotorDriver::PwmOutputImpl::Config defaultPwmOutputConfig(gpio_num_t pin, kf::u8 channel) noexcept {
         return MotorDriver::PwmOutputImpl::Config{
             .frequency_hz = 20000,
             .resolution_bits = 8,
@@ -26,17 +28,32 @@ struct PeripheryConfig final : kf::mixin::NonCopyable {
         };
     }
 
+    static constexpr WheelOdometerEncoder::Config defaultWheelOdometerEncoderConfig(gpio_num_t a, gpio_num_t b, WheelOdometerEncoder::Config::Direction positive_direction) noexcept {
+        return WheelOdometerEncoder::Config{
+            .units_per_tick = static_cast<WheelOdometerEncoder::Config::UnitType>(1),
+            .positive_direction = positive_direction,
+            .gpio_num_phase_a = static_cast<WheelOdometerEncoder::Config::GpioNumType>(a),
+            .gpio_num_phase_b = static_cast<WheelOdometerEncoder::Config::GpioNumType>(b),
+        };
+    }
+
+    static constexpr MotorDriver::Config defaultMotorDriverConfig() noexcept {
+        return MotorDriver::Config{
+            .max_input = 1000,
+            .forward_dead_zone = 10,
+            .backward_dead_zone = 10,
+        };
+    }
+
     static constexpr PeripheryConfig defaults() noexcept {
         return PeripheryConfig{
-            .motor_left_pwm_forward = createMotorPwmOutputConfig(GPIO_NUM_32, 0),
-            .motor_left_pwm_backward = createMotorPwmOutputConfig(GPIO_NUM_33, 1),
-            .motor_right_pwm_forward = createMotorPwmOutputConfig(GPIO_NUM_25, 2),
-            .motor_right_pwm_backward = createMotorPwmOutputConfig(GPIO_NUM_26, 3),
-            .motor_driver = {
-                .max_input = 1000,
-                .forward_dead_zone = 10,
-                .backward_dead_zone = 10,
-            },
+            .motor_driver_left_pwm_forward = defaultPwmOutputConfig(GPIO_NUM_32, 0),
+            .motor_driver_left_pwm_backward = defaultPwmOutputConfig(GPIO_NUM_33, 1),
+            .motor_driver_right_pwm_forward = defaultPwmOutputConfig(GPIO_NUM_25, 2),
+            .motor_driver_right_pwm_backward = defaultPwmOutputConfig(GPIO_NUM_26, 3),
+            .motor_driver = defaultMotorDriverConfig(),
+            .wheel_odometry_encoder_left = defaultWheelOdometerEncoderConfig(GPIO_NUM_36, GPIO_NUM_39, WheelOdometerEncoder::Config::Direction::CCW),
+            .wheel_odometry_encoder_right = defaultWheelOdometerEncoderConfig(GPIO_NUM_34, GPIO_NUM_35, WheelOdometerEncoder::Config::Direction::CW),
         };
     }
 };
@@ -57,14 +74,22 @@ struct Periphery final :
 
     MotorDriver motor_driver_left{
         this->config().motor_driver,
-        MotorDriver::PwmOutputImpl{this->config().motor_left_pwm_forward},
-        MotorDriver::PwmOutputImpl{this->config().motor_left_pwm_backward},
+        MotorDriver::PwmOutputImpl{this->config().motor_driver_left_pwm_forward},
+        MotorDriver::PwmOutputImpl{this->config().motor_driver_left_pwm_backward},
     };
 
     MotorDriver motor_driver_right{
         this->config().motor_driver,
-        MotorDriver::PwmOutputImpl{this->config().motor_right_pwm_forward},
-        MotorDriver::PwmOutputImpl{this->config().motor_right_pwm_backward},
+        MotorDriver::PwmOutputImpl{this->config().motor_driver_right_pwm_forward},
+        MotorDriver::PwmOutputImpl{this->config().motor_driver_right_pwm_backward},
+    };
+
+    WheelOdometerEncoder wheel_odometry_encoder_left{
+        this->config().wheel_odometry_encoder_left,
+    };
+
+    WheelOdometerEncoder wheel_odometry_encoder_right{
+        this->config().wheel_odometry_encoder_right,
     };
 
 private:
@@ -74,6 +99,9 @@ private:
 
     KF_IMPL_INITABLE(This, bool);
     bool initImpl() noexcept {
+
+        wheel_odometry_encoder_left.init();
+        wheel_odometry_encoder_right.init();
 
         if (not motor_driver_left.init()) { return false; }
         if (not motor_driver_right.init()) { return false; }
