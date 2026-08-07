e24d7d98e119ee3ce7d9891748e8f5843d001570
KiraFlux <pret2titio1n@gmail.com>
2026-08-07 18:09:58 +0300

    feat: add TransportLink (mock alias to EspnowTransport now..)

---
+++ src/botix/transport/TransportLink.hpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2026 KiraFlux
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include "botix/transport/EspnowTransport.hpp"
+
+namespace botix::transport {
+
+using TransportLink = EspnowTransport;
+
+}
